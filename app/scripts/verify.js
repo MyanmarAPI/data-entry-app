5e1b9ded86a1d6e66fa8605a1dccfac8d829e898
Nick Doiron <ndoiron@mapmeld.com>
2015-08-31 21:28:42 +0630

    look up people on the list; autocomplete on constituency names

--- app/scripts/verify.js
+++ app/scripts/verify.js
@@ -15,13 +15,17 @@ $(function () {
   }
 
   // sort unique candidates alphabetically
-  candidates = myanmarNameSort($("tr"), function(candidate) {
-    return $(candidate).find('td.name').text();
-  });
-  $("table#candidates").html(candidates);
+  var sortCandidates = function() {
+    candidates = myanmarNameSort($("tr"), function(candidate) {
+      return $(candidate).find('td.name').text();
+    });
+    $("table#candidates").html(candidates);
+  };
+
+  candidates = $("tr");
   $("span.total").text(candidates.length - 1);
 
-  $(".verify button").click(function (e) {
+  var verifyButtonRespond = function(e) {
     var candidate = $(e.currentTarget).parents("tr")[0];
     if ($(e.currentTarget).text() === "Verify") {
       $(e.currentTarget)
@@ -33,7 +37,9 @@ $(function () {
         .css({ backgroundColor: "#4a4", color: "#000" });
     }
     updateVerifyCount();
-  });
+  };
+
+  $(".verify button").click(verifyButtonRespond);
 
   updateVerifyCount = function() {
     var verifyCount = 0;
@@ -46,6 +52,43 @@ $(function () {
   };
 
   updateVerifyCount();
+
+  $.getJSON("/constituencies.json", function (constituencies) {
+    new AutoComplete('conname', constituencies);
+  });
+
+  $("button.findmore").click(function() {
+    $("button.findmore").attr("disabled", true);
+    $.getJSON("/candidate/" + $("input.findmore").val().replace("/", "*") + "?format=json", function (results) {
+      $("button.findmore").attr("disabled", false);
+      for (var r = 0; r < results.length; r++) {
+        var tr = $("<tr>");
+        tr.append($("<td class='source'></td>").text(results[r].source));
+        tr.append($("<td class='dbid'></td>").text(results[r].id));
+        tr.append($("<td class='constituency_number'></td>").text(results[r].constituency_number));
+        tr.append($("<td class='norm_id'></td>").text(results[r].norm_national_id));
+        tr.append($("<td class='name'></td>").text(results[r].full_name));
+        tr.append($("<td class='natid'></td>").text(results[r].national_id));
+        tr.append($("<td class='party'></td>").text(results[r].party));
+        tr.append($("<td class='address'></td>").text(results[r].perm_address));
+
+        if (results[r].house) {
+          tr.append($("<td class='house'></td>").text(results[r].house));
+        } else {
+          tr.append($('<td class="house"><select><option value="အမျိုးသားလွှတ်တော်">အမျိုးသားလွှတ်တော်</option><option value="ပြည်သူ့လွှတ်တော်">ပြည်သူ့လွှတ်တော်</option><option value="တိုင်းဒေသကြီး/ပြည်နယ် လွှတ်တော်">တိုင်းဒေသကြီး/ပြည်နယ် လွှတ်တော်</option></select></td>'));
+        }
+        if (results[r].verified) {
+          tr.append($("<td class='verify'><button class='verified'>Remove?</button></td>").text(results[r].address));
+        } else {
+          tr.append($("<td class='verify'><button>Verify</button></td>").text(results[r].address));
+        }
+
+        $("table").append(tr);
+      }
+      sortCandidates();
+      $(".verify button").off("click").click(verifyButtonRespond);
+    });
+  });
 
   $("button.done").click(function() {
     // update verified and unverified candidates
